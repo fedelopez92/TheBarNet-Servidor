c8ecf7309dcd0ee9c327d05653fd558f0ac3d99f
Federico López <fedelopez1492@gmail.com>
2021-05-14 00:29:21 -0300

    Agregadas rutas /usuarios: GET: /type/:id, PUT: /id, PUT: /no-confiables/:id y DELETE: /:id

--- modulos/sql/index.js
+++ modulos/sql/index.js
@@ -43,7 +43,22 @@ function traerUsuarioId(id){
 })
 }
 
-function traerUsuarioEmail(email){ 
+function traerTipoUsuario(id){ 
+    return new Promise((resolve, reject)=>{
+
+        conn.query('select tipo from usuario where id = ?', id, (e, data, fields)=>{ 
+
+        if(e != null){
+            reject(e);
+            return;
+        }
+
+        resolve(data[0]);
+    })
+})
+}
+
+/* function traerUsuarioEmail(email){ 
     return new Promise((resolve, reject)=>{
 
         conn.query('select tipo, nombre, apellido, dni, cuit, email, password, telefono, direccion, localidad, provincia, codigoPostal from usuario where email = ?', email, (e, data, fields)=>{ 
@@ -56,7 +71,7 @@ function traerUsuarioEmail(email){
         resolve(data);
     })
 })
-}
+} */
 
 function insertarUsuario(datos){ 
     return new Promise((resolve, reject)=>{
@@ -68,16 +83,69 @@ function insertarUsuario(datos){
             return;
         }
 
-        resolve("Usuario agregado !");
+        resolve("Usuario agregado");
     })
 
 })
 }
 
+function modificarUsuario(datos, id){ 
+    return new Promise((resolve, reject)=>{
+
+        conn.query('UPDATE usuario SET tipo = ?, nombre = ?, apellido = ?, dni = ?, cuit = ?, email = ?, password = ?, telefono = ?, direccion = ?, localidad = ?, provincia = ?, codigoPostal = ? WHERE id = ?', [datos[0], datos[1], datos[2], datos[3], datos[4], datos[5], datos[6], datos[7], datos[8], datos[9], datos[10], datos[11], id], (e, data, fields)=>{ 
+
+        if(e != null){
+            reject(e);
+            return;
+        }
+
+        resolve("El usuario se modificó correctamente");
+    })
+})
+}
+
+function modificarUsuarioConfiable(confiable, id){ 
+    return new Promise((resolve, reject)=>{
+
+        conn.query('UPDATE usuario SET confiable = ? WHERE id = ?', [confiable, id], (e, data, fields)=>{ 
+
+        if(e != null){
+            reject(e);
+            return;
+        }
+
+        if(confiable == "yes"){
+            resolve("Usuario confiable");
+            return;
+        }
+
+        resolve("Usuario no confiable");
+    })
+})
+}
+
+function eliminarUsuario(id){ 
+    return new Promise((resolve, reject)=>{
+
+        conn.query('DELETE FROM usuario WHERE id = ?', [id], (e, data, fields)=>{ 
+
+        if(e != null){
+            reject(e);
+            return;
+        }
+
+        resolve("Se borró el usuario");
+    })
+})
+}
+
 
 module.exports.traerUsuarios = traerUsuarios;
-module.exports.traerUsuarioEmail = traerUsuarioEmail;
 module.exports.traerUsuarioId = traerUsuarioId;
+module.exports.traerTipoUsuario = traerTipoUsuario;
 module.exports.insertarUsuario = insertarUsuario;
+module.exports.modificarUsuario = modificarUsuario;
+module.exports.modificarUsuarioConfiable = modificarUsuarioConfiable;
+module.exports.eliminarUsuario = eliminarUsuario;
 
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -29,6 +29,17 @@ router.get('/:id', (req, res)=>{
     })
 });
 
+router.get('/type/:id', (req, res)=>{
+
+    sql.traerTipoUsuario(req.params.id)
+    .then(data =>{
+        res.send(data.tipo);
+    })
+    .catch(error=>{
+        console.log(error);
+    })
+});
+
 router.post('/', (req, res)=>{
 
     let datos = [req.body.tipo, req.body.nombre, req.body.apellido, req.body.dni, req.body.cuit, req.body.email, req.body.password, req.body.telefono, req.body.direccion, req.body.localidad, req.body.provincia, req.body.codigoPostal, "yes"];
@@ -42,5 +53,44 @@ router.post('/', (req, res)=>{
     })
 });
 
+
+router.put('/:id', (req, res)=>{
+
+    let datos = [req.body.tipo, req.body.nombre, req.body.apellido, req.body.dni, req.body.cuit, req.body.email, req.body.password, req.body.telefono, req.body.direccion, req.body.localidad, req.body.provincia, req.body.codigoPostal];
+
+    sql.modificarUsuario(datos, req.params.id)
+    .then(data =>{
+        res.json(data);
+    })
+    .catch(error=>{
+        console.log(error);
+    })
+});
+
+router.put('/no-confiables/:id', (req, res)=>{
+
+    let confiable = [req.body.confiable];
+
+    sql.modificarUsuarioConfiable(confiable, req.params.id)
+    .then(data =>{
+        res.json(data);
+    })
+    .catch(error=>{
+        console.log(error);
+    })
+});
+
+
+
+router.delete('/:id', (req, res)=>{
+
+    sql.eliminarUsuario(req.params.id)
+    .then(data =>{
+        res.json(data);
+    })
+    .catch(error=>{
+        console.log(error);
+    })
+});
 
 module.exports = router;
